28f1a888cd3627379c88b47a530c8ca3a0b8fee6
Ali <abdulvahobabdullayev2002@gmail.com>
2022-08-27 07:57:32 +0500

    commid

--- assets/js/pages/index.init.js
+++ assets/js/pages/index.init.js
@@ -1,1 +1,30 @@
 var swiper=new Swiper(".testimonialSlider",{loop:!0,pagination:{el:".swiper-pagination",clickable:!0},autoplay:{delay:2500,disableOnInteraction:!1},breakpoints:{200:{slidesPerView:1,spaceBetween:10},992:{slidesPerView:1,spaceBetween:20}}}),swiper=new Swiper(".homeslider",{slidesPerView:"auto",loop:!0,spaceBetween:20,autoplay:{delay:2500,disableOnInteraction:!1}});
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+      },
+    },
+  });
